c1004a488fc1458ef2d6c0e740eb69d4f58a8c5f
Ai Tran <youraiball@gmail.com>
2023-04-05 19:27:53 -0700

    created and wrote output into txt file

--- produce_summary.py
+++ produce_summary.py
@@ -1,24 +1,34 @@
-def get_deliv_summary(day_number):
-    print("Day " + day_number)
-    print("\n")
-    the_file = open("um-deliveries-day-" + day_number + ".txt")
-    for line in the_file:
-        line = line.rstrip()
-        words = line.split('|')
+def get_deliv_summary(day_number, file_name): # defines function to generate delivery report
+    print("Day " + day_number) #prints what day report is for
+    print("\n") 
+    input_file = open(file_name) #opens file passed as parameter into function
+    deliv_summary = open("Delivery Report.txt", "a") #creates new text file if it doesn't exist
+                                                    # to write report to
+    deliv_summary.write("Day " + day_number + "\n\n") #writes day of report + new line in the text file
+    for line in input_file:    # loops through each line in parameter file
+        line = line.rstrip()   # strips that line of leading and trailing white space
+        words = line.split('|')  # breaks up that line by every "|" into a list called words
 
         #melon = words[0]
         #count = words[1]
         #amount = words[2]
-        melon, count, amount = words
+        melon, count, amount = words  #create variable for each index of the list words
+                                      # by list unpacking instead explicit indexing
 
-        print(f"Delivered {count} {melon}s for total of ${amount}")
-    the_file.close()
+        print(f"Delivered {count} {melon}s for total of ${amount}")  # outputs report line
+        deliv_summary.write(f"Delivered {count} {melon}s for total of ${amount}\n") # writes line to txt file
+    deliv_summary.write("\n") # new line in txt file
+    input_file.close()  # closes parameter file
+    deliv_summary.close() # closes new txt file
 
-get_deliv_summary("1")
+
+get_deliv_summary("1", "um-deliveries-day-1.txt") #calling function for day 1 deliveries from existing txt file
 print("\n")
-get_deliv_summary("2")
+get_deliv_summary("2", "um-deliveries-day-2.txt") #calling function for day 1 deliveries from existing txt file
 print("\n")
-get_deliv_summary("3")
+get_deliv_summary("3", "um-deliveries-day-3.txt") #calling function for day 1 deliveries from existing txt file
+
+
 
 
 # print("Day 1")
